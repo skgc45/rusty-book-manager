e4036466bf401ac14d0f20387887c883998f161e
skgc45 <s.sakachan@hotmail.co.jp>
2024-11-13 15:40:14 +0900

    done 5.2

--- adapter/src/database/mod.rs
+++ adapter/src/database/mod.rs
@@ -1,6 +1,8 @@
 use shared::config::DatabaseConfig;
 use sqlx::{postgres::PgConnectOptions, PgPool};
 
+pub mod model;
+
 fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
     PgConnectOptions::new()
         .host(&cfg.host)
@@ -14,6 +16,9 @@ fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
 pub struct ConnectionPool(PgPool);
 
 impl ConnectionPool {
+    pub fn new(pool: PgPool) -> Self {
+        Self(pool)
+    }
     // sqlx:PgPoolへの参照を取得する
     pub fn inner_ref(&self) -> &PgPool {
         &self.0
