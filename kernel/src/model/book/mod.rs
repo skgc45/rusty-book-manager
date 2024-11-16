e3782e78b95b8bb58ded0bd737334782c9607597
skgc45 <s.sakachan@hotmail.co.jp>
2024-11-16 11:02:43 +0900

    done 5.3: detail error and create macro for id(uuid)

--- kernel/src/model/book/mod.rs
+++ kernel/src/model/book/mod.rs
@@ -1,10 +1,10 @@
-use uuid::Uuid;
-
 pub mod event;
+
+use crate::model::id::BookId;
 
 #[derive(Debug)]
 pub struct Book {
-    pub id: Uuid,
+    pub id: BookId,
     pub title: String,
     pub author: String,
     pub isbn: String,
