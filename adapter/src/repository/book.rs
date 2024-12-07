0ec1e2f1108088d40e3dfa1004c33113e38ebb3c
skgc45 <s.sakachan@hotmail.co.jp>
2024-12-07 17:17:26 +0900

    done 5.7.3 to expand model

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -4,7 +4,7 @@ use kernel::{
     model::{
         book::{
             event::{CreateBook, DeleteBook, UpdateBook},
-            Book, BookListOptions,
+            Book, BookListOptions, Checkout,
         },
         id::{BookId, UserId},
         list::PaginatedList,
@@ -12,8 +12,9 @@ use kernel::{
     repository::book::BookRepository,
 };
 use shared::error::{AppError, AppResult};
+use std::collections::HashMap;
 
-use crate::database::model::book::{BookRow, PaginatedBookRow};
+use crate::database::model::book::{BookCheckoutRow, BookRow, PaginatedBookRow};
 use crate::database::ConnectionPool;
 
 #[derive(new)]
@@ -89,7 +90,16 @@ impl BookRepository for BookRepositoryImpl {
         .await
         .map_err(AppError::SpecificOperationError)?;
 
-        let items = rows.into_iter().map(Book::from).collect();
+        let book_ids = rows.iter().map(|book| book.book_id).collect::<Vec<_>>();
+        let mut checkouts = self.find_checkouts(&book_ids).await?;
+
+        let items = rows
+            .into_iter()
+            .map(|row| {
+                let checkout = checkouts.remove(&row.book_id);
+                row.into_book(checkout)
+            })
+            .collect();
 
         Ok(PaginatedList {
             total,
@@ -121,7 +131,13 @@ impl BookRepository for BookRepositoryImpl {
         .await
         .map_err(AppError::SpecificOperationError)?;
 
-        Ok(row.map(Book::from))
+        match row {
+            Some(r) => {
+                let checkout = self.find_checkouts(&[r.book_id]).await?.remove(&r.book_id);
+                Ok(Some(r.into_book(checkout)))
+            }
+            None => Ok(None),
+        }
     }
 
     async fn update(&self, event: UpdateBook) -> AppResult<()> {
@@ -176,6 +192,34 @@ impl BookRepository for BookRepositoryImpl {
     }
 }
 
+impl BookRepositoryImpl {
+    async fn find_checkouts(&self, book_ids: &[BookId]) -> AppResult<HashMap<BookId, Checkout>> {
+        let res = sqlx::query_as!(
+            BookCheckoutRow,
+            r#"
+                SELECT
+                    c.checkout_id,
+                    c.book_id,
+                    u.user_id,
+                    u.name AS user_name,
+                    c.checked_out_at
+                FROM checkouts AS c
+                INNER JOIN users AS u USING(user_id)
+                WHERE book_id = ANY($1);
+            "#,
+            book_ids as _
+        )
+        .fetch_all(self.db.inner_ref())
+        .await
+        .map_err(AppError::SpecificOperationError)?
+        .into_iter()
+        .map(|checkout| (checkout.book_id, Checkout::from(checkout)))
+        .collect();
+
+        Ok(res)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -225,6 +269,7 @@ mod tests {
             isbn,
             description,
             owner,
+            ..
         } = res.unwrap();
         assert_eq!(id, book_id);
         assert_eq!(title, "Test Title");
--- kernel/src/model/book/mod.rs
+++ kernel/src/model/book/mod.rs
@@ -1,4 +1,8 @@
-use crate::model::{id::BookId, user::BookOwner};
+use crate::model::{
+    id::{BookId, CheckoutId},
+    user::{BookOwner, CheckoutUser},
+};
+use chrono::{DateTime, Utc};
 
 pub mod event;
 
@@ -10,10 +14,18 @@ pub struct Book {
     pub isbn: String,
     pub description: String,
     pub owner: BookOwner,
+    pub checkout: Option<Checkout>,
 }
 
 #[derive(Debug)]
 pub struct BookListOptions {
     pub limit: i64,
     pub offset: i64,
+}
+
+#[derive(Debug)]
+pub struct Checkout {
+    pub checkout_id: CheckoutId,
+    pub checked_out_by: CheckoutUser,
+    pub checked_out_at: DateTime<Utc>,
 }
