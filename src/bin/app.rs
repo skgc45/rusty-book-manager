193523806f82538400db8c85ab1010c3097fa0c8
skgc45 <s.sakachan@hotmail.co.jp>
2024-12-11 21:21:30 +0900

    add jaeger for tracing

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -8,6 +8,7 @@ use anyhow::Context;
 use anyhow::{Ok, Result};
 use api::route::{auth, v1};
 use axum::{http::Method, Router};
+use opentelemetry::global;
 use registry::AppRegistryImpl;
 use shared::config::AppConfig;
 use shared::env::{which, Environment};
@@ -32,6 +33,22 @@ fn init_logger() -> Result<()> {
         Environment::Production => "info",
     };
 
+    // 環境変数の読み込み
+    let host = std::env::var("JAEGER_HOST")?;
+    let port = std::env::var("JAEGER_PORT")?;
+    let endpoint = format!("{hots}:{port}");
+
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_endpoint(endpoint)
+        .with_service_name("book-manager")
+        .with_auto_split_batch(true)
+        .with_max_packet_size(8192)
+        .install_simple()?;
+
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     // ログレベルの設定
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
 
@@ -40,10 +57,14 @@ fn init_logger() -> Result<()> {
         .with_file(true)
         .with_line_number(true)
         .with_target(false);
+    // リリースビルド(本番環境)では、JSONの構造化ログを出力する
+    // #[cfg(not(debug_assertions))]
+    let subscriber = subscriber.json(); // ログをJSON形式にできる。
 
     tracing_subscriber::registry()
         .with(subscriber)
         .with(env_filter)
+        .with(opentelemetry)
         .try_init()?;
 
     Ok(())
@@ -83,6 +104,7 @@ async fn bootstrap() -> Result<()> {
     let listener = TcpListener::bind(addr).await.unwrap();
     tracing::info!("Listening on {}", addr);
     axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .context("Unexpected error happened in server")
         .inspect_err(|e| {
@@ -92,4 +114,30 @@ async fn bootstrap() -> Result<()> {
                 "Unexpected error"
             )
         })
+}
+
+async fn shutdown_signal() {
+    fn purge_spans() {
+        global::shutdown_tracer_provider();
+    }
+
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            tracing::info!("Ctrl-Cを受信しました");
+            purge_spans();
+        },
+        _ = terminate => {
+            tracing::info!("SIGTERMを受信しました");
+            purge_spans();
+        }
+    }
 }
